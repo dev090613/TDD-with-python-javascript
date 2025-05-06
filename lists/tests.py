c8df3d0abce4e5f3da9001c90cff10d8e9fb966a
kokoro <128305501+dev090613@users.noreply.github.com>
2025-05-06 11:20:30 +0900

    Refactor home page view to user a template

--- lists/tests.py
+++ lists/tests.py
@@ -8,6 +8,4 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
 
         response = self.client.get("/")
-        self.assertContains("<title>To-Do lists</title>", response)
-        self.assertContains("<html>", response)
-        self.assertContains("</html>", response)
+        self.assertTemplateUsed(response, "worng.html")
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home_page(request):
-    response = HttpResponse("<html><title>To-Do lists</title></html>")
-    return response
+    return render(request, "home.html")
