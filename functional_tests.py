94edd498bb23da380bf2df8e7d0ddd6aefa19aee
kokoro <128305501+dev090613@users.noreply.github.com>
2025-05-06 22:47:53 +0900

    modify "home.html", "lists/views.py", unit test and FT.

--- functional_tests.py
+++ functional_tests.py
@@ -40,10 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
         # "1: Buy peacock feathers"라는 항목이 to-do 리스트에
-        self.assertTrue(
-            any(row.text == "1: Buy peacock feathers" for row in rows),
-            "New to-do item did not appear in table",
-        )
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
 
 
         self.fail("Finish the test!")
