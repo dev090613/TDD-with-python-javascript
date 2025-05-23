277c6d558021c0aba7dba3868afd5ca0754e4cb9
kokoro <128305501+dev090613@users.noreply.github.com>
2025-05-23 16:17:35 +0900

    first-cut FT

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -1,12 +1,10 @@
 import os
+import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
-from unittest import skip
-import time
 
 MAX_WAIT = 5
 
@@ -36,96 +34,12 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise
                 time.sleep(0.5)
 
-
-class NewVisitorTest(FunctionalTest):
-
-    def test_can_start_a_todo_list(self):
-
-        # Edith는 멋진 to-do app을 알게 되었다.
-        # 그녀는 홈페이지에 방문한다.
-        self.browser.get(self.live_server_url)
-
-        # 그녀는 홈페이지 title과 header이
-        # "To-Do"로 시작함을 확인
-        self.assertIn("To-Do", self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
-        self.assertIn("To-Do", header_text)
-
-        # 그녀는 to-do 항목을 입력할 수 있도록 초대 받음
-        # placeholder를 확인(<input placeholder="..">)
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
-
-        # 그녀는 "Buy peacock feathers"를 텍스트 박스에 입력
-        inputbox.send_keys("Buy peacock feathers")
-        # 엔터를 누르면, 페이지가 업데이트 되고,
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Buy peacock feathers")
-        # "Use peacock feathers to make a fly" 입력 & 엔터
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-        inputbox.send_keys("Use peacock feathers to make a fly")
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Buy peacock feathers")
-        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
-
-    def test_multiple_users_can_start_lists_at_different_urls(self):
-        # Edith starts a new to-do lists
-        self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-        inputbox.send_keys("Buy peacock feathers")
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Buy peacock feathers")
-
-        # she notices that her list has a unique URL
-        edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, "lists/.+")
-
-        # Now a new user, Francis, comes along to the site.
-
-        ## We delete all the browser's cookies
-        ## as a way of simulating a brand new user session
-        self.browser.delete_all_cookies()
-
-        # Francis visites the home page.
-        # There is no sign of Edith's list
-        self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element(By.TAG_NAME, "body").text
-        self.assertNotIn("Buy peacock feathers", page_text)
-        self.assertNotIn("make a fly", page_text)
-
-        # Francis starts a new list by entering a new item.
-        # He is less interesting than Edith...
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-        inputbox.send_keys("Buy milk")
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Buy milk")
-
-        # Francis gets his own unique URL
-        francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, "/lists/.+")
-        self.assertNotEqual(francis_list_url, edith_list_url)
-
-        page_text = self.browser.find_element(By.TAG_NAME, "body").text
-        self.assertNotIn("Buy peacock feathers", page_text)
-        self.assertIn("Buy milk", page_text)
-
-
-class LayoutAndStylingTest(FunctionalTest):
-    def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
-
-        # Her browser window is set to a very specific size
-        self.browser.set_window_size(1024, 768)
-
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-        self.assertAlmostEqual(
-            inputbox.location["x"] + inputbox.size["width"] / 2,
-            512,
-            delta=10,
-        )
-
-
-class ItemValidationTest(FunctionalTest):
-    @skip
-    def test_cannot_add_empty_list_items(self):
-        pass
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException):
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -7,4 +7,30 @@ MAX_WAIT = 5
 
 class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
-        self.fail("Write me!")
+
+        self.browser.get(self.live_server_url)
+        # 빈 항목 제출
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
+                "You can't have an empty list item",
+            )
+        )
+
+        self.browser.find_element(By.ID, "id_new_item").send_keys("Purchase milk")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Purchase milk")
+
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
+                "You can't have an empty list item",
+            )
+        )
+
+        self.browser.find_element(By.ID, "id_new_item").send_keys("Make tea")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("2: Make tea")
